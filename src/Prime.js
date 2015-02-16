c34c685b93463835cae83491cb8b7166b03f733e
Gina Contrino <g.contrino@hotmail.de>
2015-02-16 15:12:36 +0100

    Completed the prime calculator.

--- src/Prime.js
+++ src/Prime.js
@@ -6,6 +6,12 @@ function Prime() {
 
 Prime.prototype = {
 
+ /**
+  * Returns a comma seperated list of all Prime numbers up to n
+  *
+  * @param integer max
+  * @return string
+  */
   stringAllUpTo: function(max) {
 
     return this.allUpTo(max).join(', ');
@@ -13,7 +19,12 @@ Prime.prototype = {
   },
 
 
-
+ /**
+  * Returns an array of all Prime numbers up to n
+  *
+  * @param integer max
+  * @return array
+  */
   allUpTo: function(max) {
 
     return this.range(max).filter(this.isPrime);
@@ -21,7 +32,12 @@ Prime.prototype = {
   },
 
 
-
+ /**
+  * Returns whether number is prime or not
+  *
+  * @param integer number
+  * @return boolean
+  */
   isPrime: function(number) {
 
     // special case: 1 is not a prime
@@ -49,7 +65,12 @@ Prime.prototype = {
   },
 
 
-
+ /**
+  * Returns an array of numbers from 1 to max
+  *
+  * @param integer max
+  * @return array
+  */
   range: function(max) {
 
     this.rangeArray = [];
@@ -60,7 +81,35 @@ Prime.prototype = {
     }
 
     return this.rangeArray;
-  }
+  },
+
+
+ /**
+  * Returns an array of n Prime numbers
+  *
+  * @param integer max
+  * @return array
+  */
+  getFirstNPrimes: function(max) {
+
+      max = +max;
+
+      this.primesArray = [];
+
+
+      for ( var count = 2; this.primesArray.length < max ; count++ )
+      {
+
+        if (this.isPrime(count) === true)
+        {
+          this.primesArray.push(count);
+        }
+      }
+
+      return this.primesArray;
+
+  },
+
 
 };
 
--- src/findPrimeCli.js
+++ src/findPrimeCli.js
@@ -1,4 +1,20 @@
 #! /usr/local/bin/node
 
-var prime = require('./Prime');
-console.log(prime.allUpTo(process.argv[2]));
+/*
+  Prime Main File
+  ===============
+
+  Gives the first n primes as array.
+  So to get the first 5 primes, enter:
+
+    > $ node src/findPrimeCli.js 5
+
+
+  Which outputs the following array:
+
+    > [ 2, 3, 5, 7, 11 ]
+
+*/
+
+var Prime = require('./Prime');
+console.log(Prime.getFirstNPrimes(process.argv[2]));
